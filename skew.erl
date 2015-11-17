81bd48ca415a8afde9703bab8471d3fd4b264ac0
Alec Hollingsworth <alec@mailc.net>
2015-11-16 22:26:05 -0500

    Add digits/1 function.
    I'm not 100% sure [1 | List1 ++ List2] is the best way to write this but it seems to work.  Maybe it would've been more clear if I didn't separate the powers/1 and digits/1 functions after all.

--- skew.erl
+++ skew.erl
@@ -1,5 +1,5 @@
 -module(skew).
--export([powers/1]).
+-export([powers/1, digits/1]).
 
 powers(0) -> [];
 powers(1) -> [1];
@@ -7,3 +7,16 @@ powers(N) ->
     Log2 = trunc(math:log2(N + 1)),
     LargestSkew = trunc(math:pow(2, Log2)) - 1,
     [LargestSkew | powers(N - LargestSkew)].
+
+zeros(N) -> lists:duplicate(N, 0).
+
+log(N) -> trunc(math:log2(N + 1)) - 1.
+
+digits([]) -> [];
+digits([P]) -> [1 | zeros(log(P))];
+digits([P, Q]) when P == Q -> [2 | zeros(log(P))];
+digits([P, Q]) -> [1 | zeros(log(P) - log(Q) - 1) ++ digits([Q])];
+digits([P, Q, R | Powers]) when P == Q ->
+    [2 | zeros(log(P) - log(R) - 1) ++ digits([R | Powers])];
+digits([P, Q | Powers]) ->
+    [1 | zeros(log(P) - log(Q) - 1) ++ digits([Q | Powers])].
